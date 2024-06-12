ae601e826b5e97fa6b7e692751555343ea1edb5b
David Fu <davidzwfu@gmail.com>
2024-06-12 16:58:17 +0400

    Update DetailModal.tsx

--- src/components/DetailModal.tsx
+++ src/components/DetailModal.tsx
@@ -108,7 +108,7 @@ export default function DetailModal({
               <svg width="20" height="20" viewBox="0 0 24 24" role="img" data-icon="XStandard" aria-hidden="true"><path fillRule="evenodd" clipRule="evenodd" d="M10.5858 12L2.29291 3.70706L3.70712 2.29285L12 10.5857L20.2929 2.29285L21.7071 3.70706L13.4142 12L21.7071 20.2928L20.2929 21.7071L12 13.4142L3.70712 21.7071L2.29291 20.2928L10.5858 12Z" fill="currentColor"></path></svg>
             </button>
             <div className="modal-main">
-              <img className="modal__img modal__img--placeholder" src={item.billboard?.url ?? item.boxart?.url} />
+              <img className="modal__img modal__img--placeholder" src={item.boxart?.url} />
               <div className="modal-main__gradient" style={{
                 ...(exitPosition && state == 'exiting') && { 
                   opacity: 0,
